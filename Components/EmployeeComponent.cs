d0d34f377027f3cb95f6dd84fd2e19cc762ee481
LDVKhang <157005663+LDVKhang@users.noreply.github.com>
2024-05-30 18:37:19 +0700

    Add files via upload

---
+++ Components/EmployeeComponent.cs
@@ -0,0 +1,20 @@
+
+using FinalProject.Models;
+
+public partial class EmployeeComponent : UserControl
+{
+    public EmployeeComponent()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DisplayEmployee(Employee employee)
+    {
+        // Code to display employee details in the component
+    }
+}
